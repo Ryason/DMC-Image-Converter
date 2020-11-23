95573282e4cbfb1ceb2a40c5133817c47b197cea
Ryason <46761112+Ryason@users.noreply.github.com>
2020-11-23 18:44:54 +0000

    starting to build the pdf export feature

---
+++ DMCConverter/ExportAsPDF.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+namespace DMCConverter
+{
+    public class ExportAsPDF
+    {
+        public void Create(string imagePath)
+        {
+            PdfDocument document = new PdfDocument();
+
+            document.AddPage();
+
+            XGraphics gfx = XGraphics.FromPdfPage(document.Pages[0]);
+
+            XImage image = XImage.FromFile(imagePath);
+
+            gfx.DrawImage(image, 0,0);
+            
+            string filename = "test.pdf";
+
+            document.Save(filename);
+
+            //openms the pdf for the user
+            Process.Start("test.pdf");
+        }
+
+
+    }
+}
